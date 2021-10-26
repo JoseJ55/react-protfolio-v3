60d04b3dc936db9f22e6251399ec9af8444aec9b
Jose Jimenez <josejimenez826@gmail.com>
2021-10-25 19:12:26 -0700

    finished the first part of contact section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./style.css";
 
+// import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+
 function Footer(){
     return (
         <div id="footer">
             <div id="contact">
-                <h2>GET IN TOUCH</h2>
-                <p>Email: <a href="josejimenez826@gmail.com">josejimenez826@gmail.com</a></p>
-                <p>Phone: 1-(760)-221-2790</p>
+                <h2 id="contactTitle">GET IN TOUCH</h2>
+                <p className="contactItem">Email: <a href="josejimenez826@gmail.com">josejimenez826@gmail.com</a></p>
+                <p className="contactItem">Phone: 1-(760)-221-2790</p>
                 <div id="contactIcons">
-                    <a href="#">git</a>
-                    <a href="#">linkedin</a>
+                    <a href="https://github.com/JoseJ55"><AiFillGithub className="icon" size={28}  color="white"/></a>
+                    <a href="https://www.linkedin.com/in/jose-jimenez-238453169"><AiFillLinkedin className="icon" size={28} color="white"/></a>
                 </div>
             </div>
             <div id="email">
