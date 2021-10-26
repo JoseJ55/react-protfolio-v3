ee6098c76937d52770a025026c3a25ddc2aa4a38
Jose Jimenez <josejimenez826@gmail.com>
2021-10-26 14:43:30 -0700

    worked on sending emails with js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import emailjs from "emailjs-com";
 
 // import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function Footer(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [subject, setSubject] = useState("");
+    const [text, setText] = useState("");
+
+    const sendData = () => {
+        // e.preventDefault();
+        // console.log(name);
+        // console.log(email);
+        // console.log(phone);
+        // console.log(subject);
+        // console.log(text);
+        let mail = {
+            Name: name,
+            Email: email,
+            Phone: phone,
+            Subject: subject,
+            Text: text
+        }
+        
+        emailjs.sendForm("service_34856gt", "template_6oej2fw", mail, "user_XiAqAO8SE5exdjP8TwO0u")
+        .then((result) => {
+            console.log(result)
+            console.log(name)
+        }, (err) => {
+            console.log(err.text)
+        })
+    }
+
     return (
         <div id="footer">
             <div id="contact">
@@ -18,13 +49,44 @@ function Footer(){
             </div>
 
             <div id="email">
-                <input id="emailName" className="emailItem" type="text" placeholder="Name"/>
-                <input id="emailEmail" className="emailItem" type="text" placeholder="Email"/>
-                <input id="emailPhone" className="emailItem" type="text" placeholder="Phone"/>
-                <input id="emailSub" className="emailItem" type="text" placeholder="Subject"/>
-                <textarea id="emailMessage" className="emailItem" type="text" placeholder="Type you message here..." rows="7"/>
-                <input id="emailBtn" type="button" value="Submit"/>
-
+                <input 
+                    id="emailName" 
+                    className="emailItem" 
+                    type="text" 
+                    placeholder="Name" 
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input 
+                    id="emailEmail" 
+                    className="emailItem" 
+                    type="text" 
+                    placeholder="Email" 
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                    id="emailPhone" 
+                    className="emailItem" 
+                    type="text" 
+                    placeholder="Phone" 
+                    onChange={(e) => setPhone(e.target.value)}
+                />
+                <input 
+                    id="emailSub" 
+                    className="emailItem" 
+                    type="text" 
+                    placeholder="Subject" 
+                    onChange={(e) => setSubject(e.target.value)}
+                />
+                <textarea 
+                    id="emailMessage" 
+                    className="emailItem" 
+                    type="text" 
+                    placeholder="Type you message here..." 
+                    rows="7" 
+                    onChange={(e) => setText(e.target.value)}
+                />
+                
+                <input id="emailBtn" type="button" value="Submit" onClick={() => sendData()}/>
             </div>
         </div>
     )
