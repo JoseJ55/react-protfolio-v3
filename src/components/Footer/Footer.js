607d42c23230486d3795c0934b41f60a2fdd2b58
Jose Jimenez <josejimenez826@gmail.com>
2021-10-25 21:07:30 -0700

    finished the html for second part of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,20 +9,21 @@ function Footer(){
         <div id="footer">
             <div id="contact">
                 <h2 id="contactTitle">GET IN TOUCH</h2>
-                <p className="contactItem">Email: <a href="josejimenez826@gmail.com">josejimenez826@gmail.com</a></p>
+                <p className="contactItem">Email: <a href="mailto:josejimenez826@gmail.com">josejimenez826@gmail.com</a></p>
                 <p className="contactItem">Phone: 1-(760)-221-2790</p>
                 <div id="contactIcons">
                     <a href="https://github.com/JoseJ55"><AiFillGithub className="icon" size={28}  color="white"/></a>
                     <a href="https://www.linkedin.com/in/jose-jimenez-238453169"><AiFillLinkedin className="icon" size={28} color="white"/></a>
                 </div>
             </div>
+
             <div id="email">
-                <input type="text" placeholder="Name"/>
-                <input type="text" placeholder="Email"/>
-                <input type="text" placeholder="Phone"/>
-                <input type="text" placeholder="Subject"/>
-                <input type="text" placeholder="Type you message here..."/>
-                <input type="button" value="Submit"/>
+                <input id="emailName" className="emailItem" type="text" placeholder="Name"/>
+                <input id="emailEmail" className="emailItem" type="text" placeholder="Email"/>
+                <input id="emailPhone" className="emailItem" type="text" placeholder="Phone"/>
+                <input id="emailSub" className="emailItem" type="text" placeholder="Subject"/>
+                <textarea id="emailMessage" className="emailItem" type="text" placeholder="Type you message here..." rows="7"/>
+                <input id="emailBtn" type="button" value="Submit"/>
 
             </div>
         </div>
