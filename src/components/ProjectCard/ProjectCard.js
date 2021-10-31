135c1452ead6bda73403892e60b24d6890338a56
Jose Jimenez <josejimenez826@gmail.com>
2021-10-31 11:17:28 -0700

    added link for more info on website and need to change colors for website

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,40 +1,27 @@
 import React, { useState } from "react";
 import "./style.css";
-import { useHistory } from "react-router-dom";
 import ProjectPopUp from "../ProjectPopUp/ProjectPopUp";
-
-// change names here for pictures.
-// import image1 from "./../../assets/images/googleBooksHome.png";
-// import image2 from "./../../assets/images/teamProfileGen.png";
-// import image3 from "./../../assets/images/weatherDashboard.png";
 
 function ProjectCard({ title, images, links, desc }) {
     const [popUp, setPopUp] = useState(false);
     // This is the project card component where it checks if an item is in an
     // array and renders the spcific card.
-    // console.log(images)
-    let history = useHistory();
-    const changeToProject = (files) => {
-        let file = files.replace(/\s/g, '');
-        history.push(`/${file}`);
-    }
     
     return (
         <div className="projectContainer">
             <p 
                 className="projectTitle" 
-                onClick={() => setPopUp(true)}//changeToProject(title)}
+                onClick={() => setPopUp(true)}
                 >{title}</p>
             <input 
                 type="button" 
                 className="project"
-                onClick={() => setPopUp(true)}//changeToProject(title)}
+                onClick={() => setPopUp(true)}
                 style={{
                     backgroundImage: `url(${images[0]})`
                 }}
             />
             {popUp ? <ProjectPopUp popUp={popUp} setPopUp={setPopUp} title={title} link={links} desc={desc}/>: <div></div>}
-            
         </div>
     )
 }
--- src/components/ProjectPopUp/ProjectPopUp.js
+++ src/components/ProjectPopUp/ProjectPopUp.js
@@ -2,38 +2,37 @@ import React from "react";
 import "./style.css";
 import { useSpring, animated } from "react-spring";
 import { AiOutlineClose } from "react-icons/ai";
+import { useHistory } from "react-router-dom";
 
-function ProjectPopUp({ popUp, setPopUp, title, link, desc }) {
-    // const [popUp, setPop] = useState(false);
+function ProjectPopUp({ popUp, setPopUp, title, desc }) {
+    let history = useHistory();
+    const changeToProject = (files) => {
+        let file = files.replace(/\s/g, '');
+        history.push(`/${file}`);
+    }
+
     const animation = useSpring({
-        // opacity: popUp ? 1 : 0,
-        // marginTop: popUp ? 0 : -500,
         from: {marginTop: -500},
         to: {marginTop: 0},
         duration: 2000,
     })
 
     return (
-        
         <div id="popUp">
             <animated.div id="popUpArea" style={animation}>
-                {/* <div id="popUpArea"> */}
-                    <button 
-                        type="button" 
-                        id="popUpExit"
-                        onClick={() => setPopUp(false)} 
-                        // value="show"
-                    ><AiOutlineClose size={28}/></button>
+                <button 
+                    type="button" 
+                    id="popUpExit"
+                    onClick={() => setPopUp(false)} 
+                ><AiOutlineClose size={28}/></button>
 
-                    <h2>{title}</h2>
-                    {/* change desc to a sorter version so it can fit on screen better. */}
-                    <p>{desc}</p>
+                <h2>{title}</h2>
+                {/* change desc to a sorter version so it can fit on screen better. */}
+                <p>{desc}</p>
 
-                    <button type="button" value="full" id="popUpMore">More Info</button>
-                {/* </div> */}
+                <button type="button" value="full" id="popUpMore" onClick={() => changeToProject(title)}>More Info</button>
             </animated.div>
         </div>
-        
     )
 }
 
