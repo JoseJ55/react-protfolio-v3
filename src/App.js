377ee2e81ee659adb6ac3ff42899cdf73ee38d51
Jose Jimenez <josejimenez826@gmail.com>
2021-11-12 12:22:23 -0800

    changed icon and added title and descriptions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import { Helmet } from "react-helmet";
 
 import Home from "./pages/Home";
 import Project from "./pages/Project";
@@ -11,6 +12,12 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Jose's Portfolio</title>
+        <link real="cononical" href="https://portfolio826.herokuapp.com/" />
+        <meta name="description" content="Jose Jimenez' portfolio showcasing projects and skills"/>
+      </Helmet>
       {projects.map((project) => {
         return (
           <Route exact path={`/${project.title.replace(/\s/g, '')}`}>
